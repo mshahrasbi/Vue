0a1876b9655d12ef9b1473c47f105206af94aa3d
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-20 11:27:17 -0700

    initial commit

---
+++ section03/lists/app.js
@@ -0,0 +1,12 @@
+
+new Vue({
+    el: '#app',
+
+    data: {
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Anna', age: 'Unkown', color: 'blue'}
+        ]
+    }
+});
