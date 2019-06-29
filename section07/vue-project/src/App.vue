307c666eccce21f40827713ef497ea12017bb829
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-06-28 17:17:25 -0700

    complete section 7

---
+++ section07/vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-offset-3">
+        <app-servers></app-servers>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ section07/vue-project/src/ServerStatus.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <p>Server status: {{ status  }} </p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'critical'
+            };
+        },
+        methods: {
+            changeStatus() {
+                if (this.status === 'normal') {
+                    this.status = 'criticla'
+                } else {
+                    this.status = 'normal'
+                }
+            }
+        }
+    }
+</script>
+
