1df51dc7322a8c70fe5299695dfe04862bb04088
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-07-08 15:00:52 -0700

    comminucating between components

---
+++ section08/my-project/src/components/User.vue
@@ -0,0 +1,53 @@
+
+<template>
+    <div class="component">
+        <h3>The User Component</h3>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change My Name</button>
+        <p>Name is {{ name }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    v-bind:name="name" 
+                    @nameWasReset="name=$event"
+                    :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEditted="age=$event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import UserDetail from './UserDetail.vue';
+import UserEdit from './UserEdit.vue';
+
+export default {
+    data: function() {
+        return {
+            name: 'Mohammad',
+            age: 27
+        };
+    },
+    components: {
+        appUserDetail: UserDetail,
+        appUserEdit: UserEdit
+    },
+    methods: {
+        changeName() {
+            this.name = 'Mahrou';
+        }
+    }    
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ section08/my-project/src/components/UserDetail.vue
@@ -0,0 +1,42 @@
+
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <hr>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    //props: ['name'],
+    props: {
+        name: {
+            type: String
+        },
+        userAge: Number
+    },
+    methods: {
+        switchName() {
+            return this.name.split("").reverse().join("");
+        },
+
+        resetName() {
+            this.name = "Mohammad";
+            this.$emit('nameWasReset', this.name );
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
+
