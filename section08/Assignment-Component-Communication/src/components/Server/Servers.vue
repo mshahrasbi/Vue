34ac3660f701efa390930a35b17dbd7a232e62b3
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-07-12 17:32:14 -0700

    section 8

---
+++ section08/Assignment-Component-Communication/src/components/Server/Servers.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server v-for="server in servers" v-bind:key="server.id" :server="server" ></app-server>
+        </ul>
+    </div>
+</template> 
+
+<script>
+
+    import Server from './server.vue';
+
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Criticall'},
+                    {id: 3, status: 'Unknown'},
+                    {id: 4, status: 'Normal'}
+                ]
+            }
+        },
+
+        components: {
+            appServer: Server
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ section08/Component-Communication/src/components/UserDetail.vue
@@ -0,0 +1,49 @@
+
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <hr>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from '../main';
+
+    export default {
+        //props: ['name'],
+        props: {
+            name: {
+                type: String
+            },
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+
+            resetName() {
+                this.name = "Mohammad";
+                this.$emit('nameWasReset', this.name );
+            }
+
+        },
+        created() {
+            eventBus.$on('ageWasEditted', (age) => {
+                this.userAge = age
+            });
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
+
