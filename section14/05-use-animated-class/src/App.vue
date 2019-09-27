3b7805eaac6e24f4e35c0bc85786ef51bea0ffed
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-09-27 14:28:27 -0700

    Animations and Transitions

---
+++ section14/05-use-animated-class/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class='container'> 
+    <div class='row'>
+      <div class='col-xs-12 col-sm-8 col-sm-offser-2 col-md-6 col-md-offset-3'>
+        <h1>Animations</h1>
+
+        <hr>
+
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br></br>
+
+        <transition name="fade" >
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <hr>
+        <transition name="slide" type="animation" appear>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <hr>
+        <transition 
+            enter-active-class="animated bounce"
+            leave-active-class="animated shake" >
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: true
+      }
+    }
+  }
+</script>
+
+<style>
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    /* opacity: 1; */
+
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity 0.5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0px);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0px);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+</style>
