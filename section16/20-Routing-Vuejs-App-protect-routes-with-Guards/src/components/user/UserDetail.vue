dc731051f558972790e68f2e9bb27c499a0b86b8
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2020-01-03 16:19:32 -0700

    routing

---
+++ section16/20-Routing-Vuejs-App-protect-routes-with-Guards/src/components/user/UserDetail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User loaded has Id: {{ $route.params.id }}</p>
+
+        <!-- <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-primary">Edit User</router-link> or -->
+        <!-- <router-link tag="button" :to="{ name: 'userEdit', params: { id: $route.params.id } }" class="btn btn-primary">Edit User</router-link> -->
+        <router-link 
+            tag="button" 
+            :to="link" 
+            class="btn btn-primary">Edit User</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                link: { 
+                    name: 'userEdit', 
+                    params: { 
+                        id: this.$route.params.id 
+                    } , 
+                    query: { 
+                        locale: 'en', 
+                        q: 100 
+                    },
+                    hash: '#data' 
+                }
+            }
+        },
+        
+        beforeRouteEnter(to, from, next) {
+            // if user authenticated for example
+            if (true) {
+                next();
+            } else {
+                next(false);
+            }            
+        }
+    }
+</script>
---
+++ section16/21-Routing-Vuejs-App-load-routes-lazily/src/components/user/User.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }}</p>
+        <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+
+        <hr>
+        <router-view></router-view>
+    </div>    
+</template>
+
+
+<script>
+    export default {
+        methods: {
+            navigateToHome() {
+                // this.$router.push('/'); or
+                // this.$route.push({ path: '/' }); or
+                this.$router.push({ name: 'home' });
+            }
+        }
+        
+    }
+</script>
